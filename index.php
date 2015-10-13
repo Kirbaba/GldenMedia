cd0eb4547717019d90329548b8a0386e019f517e
Vladislav <v.kirbaba@gmail.com>
2015-10-13 17:02:37 +0300

    Nav + head slider

--- index.php
+++ index.php
@@ -12,7 +12,65 @@
     <?php wp_head(); ?>
 </head>
 <body>
-	
+	<nav class="navbar navbar-default navbar-fixed-top navigation" role="navigation">
+		<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>		      
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		      	     
+				<ul class="nav navbar-nav navbar-center navigation__list">		      
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">КОНЦЕРТНЫЙ ГРАФИК</a></li>
+					<li><a href="#">ДИСКОГРАФИЯ</a></li>
+					<li><a href="#">КАТАЛОГ ПЕСЕН</a></li>
+					<li><a href="#">ФОТО</a></li> 
+					<li><a href="#">ВИДЕО</a></li>
+					<li><a href="#">СМИ</a></li>
+					<li><a href="#">СМИ</a></li>
+					<li><a href="#">ОРГАНИЗАЦИЯ КОНЦЕРТОВ</a></li>  		        		       
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>	
+	<header class="header">
+		<div class="fotorama" data-width="100%"  data-ratio="1400/822" data-fit="cover">
+			<div data-img="<?php bloginfo('template_directory'); ?>/img/Layer-5.png">
+				<div class="header__text">
+					<h4>GOLDEN MEDIA PRESENT</h4>
+					<h1>Rihanna</h1>
+					<h4>НОВЫЙ КОНЦЕРТНЫЙ ТУР ПО РОССИИ И КАЗАХСТАНУ</h4>
+				</div>
+				<div class="header__mouse">
+					<p>КРУТИТЕ МЫШКУ ВНИЗ</p>
+				</div>
+			</div>
+			<div data-img="<?php bloginfo('template_directory'); ?>/img/Layer-5.png">
+			<div class="header__text">
+					<h4>GOLDEN MEDIA PRESENT</h4>
+					<h1>Slide #2</h1>
+					<h4>НОВЫЙ КОНЦЕРТНЫЙ ТУР ПО РОССИИ И КАЗАХСТАНУ</h4>
+				</div>
+				<div class="header__mouse">
+					<p>КРУТИТЕ МЫШКУ ВНИЗ</p>
+				</div></div>
+			<div data-img="<?php bloginfo('template_directory'); ?>/img/Layer-5.png">
+			<div class="header__text">
+					<h4>GOLDEN MEDIA PRESENT</h4>
+					<h1>Slide #3</h1>
+					<h4>НОВЫЙ КОНЦЕРТНЫЙ ТУР ПО РОССИИ И КАЗАХСТАНУ</h4>
+				</div>
+				<div class="header__mouse">
+					<p>КРУТИТЕ МЫШКУ ВНИЗ</p>
+				</div></div>
+		</div>
+	</header>
     
 <?php wp_footer(); ?>
 </body>
