dc53071e56b7c3414ea4174cc895cbaf30f48b88
Vladislav <v.kirbaba@gmail.com>
2015-10-14 11:47:31 +0300

    Афиша

--- index.php
+++ index.php
@@ -40,6 +40,9 @@
 		</div><!-- /.container-fluid -->
 	</nav>	
 	<header class="header">
+		<div class="header__logo">
+			<h3><span>GOLDEN</span>MEDIA</h3>
+		</div>
 		<div class="fotorama" data-width="100%"  data-ratio="1400/822" data-fit="cover">
 			<div data-img="<?php bloginfo('template_directory'); ?>/img/Layer-5.png">
 				<div class="header__text">
@@ -71,6 +74,96 @@
 				</div></div>
 		</div>
 	</header>
+
+	<section class="showbill">
+		<div class="container">
+			<h1 class="block_title">АФИША</h1>
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="showbill__item">
+						<div class="showbill__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/best.png" alt="">
+						</div>						
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 p5">
+							<div class="showbill__item--time">
+								<h3><span>29</span>апреля</h3>
+								<p>21:00</p>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+							<div class="showbill__item--info">
+								<h2>Артур Бэст</h2>
+								<p>театр Надежды Бабкиной "Русская песня"</p>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="showbill__item">
+						<div class="showbill__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/pozner.png" alt="">
+						</div>						
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 p5">
+							<div class="showbill__item--time">
+								<h3><span>08</span>апреля</h3>
+								<p>21:00</p>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+							<div class="showbill__item--info">
+								<h2>Владимир Познер</h2>
+								<p>Московский Дом Музыки</p>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="showbill__item">
+						<div class="showbill__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/ri.png" alt="">
+						</div>						
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 p5">
+							<div class="showbill__item--time">
+								<h3><span>10</span>апреля</h3>
+								<p>21:00</p>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+							<div class="showbill__item--info">
+								<h2>Rihanna</h2>
+								<p>СК “ОЛИМПИЙСКИЙ”</p>
+							</div>
+						</div>
+					
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+					<div class="showbill__item">
+						<div class="showbill__item--img">
+							<img src="<?php bloginfo('template_directory'); ?>/img/sati.png" alt="">
+						</div>						
+						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 p5">
+							<div class="showbill__item--time">
+								<h3><span>17</span>апреля</h3>
+								<p>21:00</p>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+							<div class="showbill__item--info">
+								<h2>Сати Казанова</h2>
+								<p>КЗ “РОССИЯ”</p>
+							</div>
+						</div>					
+					</div>
+				</div>			
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 col-lg-offset-4 col-md-offset-4 col-sm-offset-3">
+				<div class="showbill__more"><a href="#">СМОТРЕТЬ ДАЛЬШЕ АФИШУ</a></div>
+			</div>
+		</div>
+	</section>
     
 <?php wp_footer(); ?>
 </body>
