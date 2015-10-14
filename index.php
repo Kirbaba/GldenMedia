2752c39a89d3c9e5b19ed9b145031c2daf480e91
Vladislav <v.kirbaba@gmail.com>
2015-10-14 16:16:58 +0300

    Десктоп. Финиь

--- index.php
+++ index.php
@@ -407,6 +407,25 @@
 			</div>
 		</div>
 	</section>
+
+	<footer class="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-lg-offset-1 col-md-4 col-md-offset-1 col-sm-6 col-xs-12">
+					<p><?php echo get_theme_mod('address_textbox'); ?></p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+					<a href="<?php echo get_theme_mod('fb_textbox'); ?>"><i class="fa fa-facebook"></i></a>
+					<a href="<?php echo get_theme_mod('tw_textbox'); ?>"><i class="fa fa-twitter"></i></a>
+					<a href="<?php echo get_theme_mod('vk_textbox'); ?>"><i class="fa fa-vk"></i></a>
+					<a href="<?php echo get_theme_mod('pi_textbox'); ?>"><i class="fa fa-pinterest"></i></a>
+					<a href="<?php echo get_theme_mod('gp_textbox'); ?>"><i class="fa fa-google-plus"></i></a>
+					<a href="<?php echo get_theme_mod('dr_textbox'); ?>"><i class="fa fa-dribbble"></i></a>
+					<a href="<?php echo get_theme_mod('li_textbox'); ?>"><i class="fa fa-linkedin"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
     
 
 	<script type="text/javascript">
